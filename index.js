c2dd4c0bb8bd0ed92a9f824a564b2b1cf0d02009
TH2FEAR <80583960+TH2FEAR@users.noreply.github.com>
2025-05-05 23:12:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const dictionaryPath = './dictionary.json';
-
+const dictionaryPath = './dictionary.txt';  
 function getDictionaryData() {
   const data = fs.readFileSync(dictionaryPath, 'utf-8');
-  return JSON.parse(data);
+  // Satırları ayırıyoruz, boşlukları temizliyoruz ve kelimeleri bir diziye çeviriyoruz
+  return data.split('\n').map(word => word.trim().toLowerCase());
 }
 
 app.get('/api/check', (req, res) => {
   const word = req.query.kelime;
 
   if (!word) {
-    return res.status(400).json({ error: 'kelime parametresi eksik' });
+    return res.status(400).json({ error: 'Kelime parametresi eksik!' });
   }
 
   const dictionary = getDictionaryData();
   const isValidWord = dictionary.includes(word.toLowerCase());
 
-  res.json({ valid: isValidWord });
+  res.json({ valid: isValidWord });  
 });
 
 app.listen(port, () => {
